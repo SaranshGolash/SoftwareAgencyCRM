978a1748f4cc9fb4e824ca546e7ee851a0ddad41
SaranshGolash <129923993+SaranshGolash@users.noreply.github.com>
2026-06-11 09:08:10 +0530

    Made styling changes to the HeaderBody Component

--- client/src/components/HeaderBody.jsx
+++ client/src/components/HeaderBody.jsx
@@ -2,26 +2,40 @@ import React from "react";
 import Button from "./Button";
 function HeaderBody() {
 
+    const HeaderStyle = {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'start',
+        flexFlow: 'column nowrap',
+        gap: '10px'
+    };
+
     const headerBodyStyle = {
         backgroundColor: 'rgba(255, 255, 255, 1)',
         color: 'black',
         paddingTop: '10px',
+        fontFamily: 'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
+        fontWeight: 'bold',
+        fontSize: '30px'
     };
 
     const btnStyle = {
         backgroundColor: '#60E1CB',
         color: 'white',
         padding: '10px',
+        paddingLeft: '20px',
+        paddingRight: '20px',
         border: 'none',
         borderRadius: '20px',
         cursor: 'pointer',
-        margin: '5px',
+        margin: 'none',
+        fontSize:'14px',
     };
 
     return (
         <div className="main">
-            <div className="header-body">
-                <h1 style={headerBodyStyle}>We help you create your <span style={{color:"#60E1CB"}}>website</span></h1>
+            <div className="header-body" style={HeaderStyle}>
+                <h1 style={headerBodyStyle}>We help you create<br/>your <span style={{color:"#60E1CB"}}>website</span></h1>
                 <p>We are a team of passionate developers who love building websites. We help businesses create stunning, user-friendly, and responsive websites that stand out from the competition.</p>
                 <Button textbtn={"Get Started"} href={'/services'} style={btnStyle}/>
             </div>
